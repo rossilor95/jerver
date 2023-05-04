02b14534f118d427f05f2f709e4390f352973b9f
rossilor95 <rossilor95@outlook.it>
2023-05-04 23:36:26 +0200

    feat: add HttpStatus

---
+++ src/main/java/com/github/rossilor95/jerver/http/HttpStatus.java
@@ -0,0 +1,33 @@
+package com.github.rossilor95.jerver.http;
+
+public enum HttpStatus {
+    OK(200, "OK"),
+    BAD_REQUEST(400, "Bad Request"),
+    NOT_FOUND(404, "Not Found"),
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error"),
+    NOT_IMPLEMENTED(501, "Not Implemented");
+
+    private final int value;
+    private final String reasonPhrase;
+
+    HttpStatus(int value, String reasonPhrase) {
+        this.value = value;
+        this.reasonPhrase = reasonPhrase;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getReasonPhrase() {
+        return reasonPhrase;
+    }
+
+    /**
+     * Return a string representation of this status code.
+     */
+    @Override
+    public String toString() {
+        return this.value + " " + this.getReasonPhrase();
+    }
+}
