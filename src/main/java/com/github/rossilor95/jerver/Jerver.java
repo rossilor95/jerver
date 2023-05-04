4c3203ea8cb56b8d6e022f45893e1194acfd09dc
rossilor95 <rossilor95@outlook.it>
2023-05-04 22:27:46 +0200

    feat: project setup

---
+++ src/main/java/com/github/rossilor95/jerver/Jerver.java
@@ -0,0 +1,9 @@
+package com.github.rossilor95.jerver;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Jerver {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Jerver!");
+    }
+}
