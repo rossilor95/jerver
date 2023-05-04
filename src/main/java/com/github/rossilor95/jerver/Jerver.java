ceb5a800ceb2aba35be1da1ac621463e827ae7a8
rossilor95 <rossilor95@outlook.it>
2023-05-05 00:27:07 +0200

    feat: add first web server implementation

--- src/main/java/com/github/rossilor95/jerver/Jerver.java
+++ src/main/java/com/github/rossilor95/jerver/Jerver.java
@@ -3,10 +3,42 @@ package com.github.rossilor95.jerver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Jerver {
     private static final Logger logger = LoggerFactory.getLogger(Jerver.class);
+    private static final int DEFAULT_PORT = 8080;
+
+    private int port;
+
+    public Jerver(int port) {
+        this.port = port;
+    }
 
     public static void main(String[] args) {
-        logger.info("Welcome to Jerver!");
+        var jerver = new Jerver(DEFAULT_PORT);
+        jerver.start();
+    }
+
+    private void start() {
+        try (var serverSocket = new ServerSocket(port)) {
+            logger.info("Jerver started on port {}", port);
+            while(true) {
+                Socket clientSocket = serverSocket.accept();
+                logger.info("Client connected: {}", clientSocket.getInetAddress());
+            }
+        } catch (IOException e) {
+            logger.error("Error starting server: {}", e.getMessage());
+        }
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
     }
 }
