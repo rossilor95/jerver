5959cc7e119a8cbfcde5d9eb88fd954cb419b384
rossilor95 <rossilor95@outlook.it>
2023-05-12 00:34:44 +0200

    feat: add HttpMethod

---
+++ src/main/java/com/github/rossilor95/jerver/http/HttpMethod.java
@@ -0,0 +1,16 @@
+package com.github.rossilor95.jerver.http;
+
+/**
+ * An enumeration representing the supported HTTP request methods.
+ */
+public enum HttpMethod {
+    /**
+     * The GET method requests a representation of the specified resource.
+     */
+    GET,
+
+    /**
+     * The HEAD method asks for a response identical to a GET request, but without the response body.
+     */
+    HEAD
+}
