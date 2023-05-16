d54c44c367739b7103b23257f4afb4c1d77e5faa
rossilor95 <rossilor95@outlook.it>
2023-05-16 23:52:59 +0200

    refactor: change type to HttpRequest headers field

--- src/main/java/com/github/rossilor95/jerver/http/HttpRequest.java
+++ src/main/java/com/github/rossilor95/jerver/http/HttpRequest.java
@@ -1,9 +1,9 @@
 package com.github.rossilor95.jerver.http;
 
-import java.util.List;
+import java.util.Map;
 
 /**
  * Represents an HTTP request, consisting of a {@link RequestLine} and a list of headers.
  */
-record HttpRequest(RequestLine requestLine, List<String> headers) {
+record HttpRequest(RequestLine requestLine, Map<String, String> headers) {
 }
