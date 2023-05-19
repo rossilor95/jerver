1a308567b0c2a1e44c0a35802a8748fece06e1db
rossilor95 <rossilor95@outlook.it>
2023-05-19 23:03:04 +0200

    refactor: add unit test to HttpRequestParser

---
+++ src/test/java/com/github/rossilor95/jerver/http/HttpRequestParserTest.java
@@ -0,0 +1,36 @@
+package com.github.rossilor95.jerver.http;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class HttpRequestParserTest {
+    private final HttpRequestParser httpRequestParser = new HttpRequestParser();
+
+    @Test
+    void shouldParseRequest() {
+        // Given:
+        var testRequest = """
+                GET /index.html HTTP/1.1
+                Host: example.com
+                Accept: text/html
+                """;
+        var expectedRequestLine = new RequestLine(HttpMethod.GET, URI.create("/index.html"), "HTTP/1.1");
+        var expectedHeaders = Map.of("Host", "example.com", "Accept", "text/html");
+        var expectedHttpRequest = new HttpRequest(expectedRequestLine, expectedHeaders);
+        var inputStream = new ByteArrayInputStream(testRequest.getBytes(StandardCharsets.UTF_8));
+
+        // When:
+        var parsedRequest = httpRequestParser.parse(inputStream);
+
+        // Then:
+        assertEquals(expectedHttpRequest, parsedRequest);
+    }
+
+}
